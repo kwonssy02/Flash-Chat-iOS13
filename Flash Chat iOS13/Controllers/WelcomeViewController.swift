2207e0ef7adf9fab5e60f18556a67d2bda3e3339
Hyukchan Kwon <kwonssy02@gmail.com>
2020-04-14 23:34:58 +0900

    add cocoapods

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -15,6 +15,8 @@ class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+//        titleLabel.text = "⚡️FlashChat"
+        
         titleLabel.text = ""
         var charIndex = 0
         let titleText = "⚡️FlashChat"
